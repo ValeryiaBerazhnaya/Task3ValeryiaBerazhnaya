c54bbda68319fbfbc4b755a272d0a1c87a3220f1
valeryiaBerazhnaya <valeryia.berazhnaya@student.ehu.lt>
2025-03-14 18:49:08 +0300

    2

---
+++ Task3/Options.cs
@@ -0,0 +1,13 @@
+﻿using CommandLine;
+
+namespace Task3
+{
+    public class Options
+    {
+        [Option('h', "help", Required = false, HelpText = "Display help information.")]
+        public bool Help { get; set; }
+
+        [Value(0, MetaName = "dice", HelpText = "List of dice sides separated by commas.")]
+        public IEnumerable<string> Dice { get; set; }
+    }
+}
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,26 +1,30 @@
-﻿namespace Task3
+﻿using CommandLine;
+
+namespace Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Contains("--help") || args.Length == 0)
             {
+                Console.WriteLine("Help information:");
                 Console.WriteLine("Usage: Task3.exe <dice1> <dice2> [<dice3> ...]");
                 Console.WriteLine("Example: Task3.exe 2,2,4,4,9,9 6,8,1,1,8,6 7,5,3,7,5,3");
                 return;
             }
 
-            try
-            {
-                Dice[] dices = DiceParser.Parse(args);
-                GameTable gameTable = new GameTable(dices);
-                gameTable.Play();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(options =>
+                {
+                    Dice[] dices = DiceParser.Parse(options.Dice.ToArray());
+                    GameTable gameTable = new GameTable(dices);
+                    gameTable.Play();
+                })
+                .WithNotParsed(errors =>
+                {
+                    Console.WriteLine("Invalid input. Use --help for more information.");
+                });
         }
     }
 }
